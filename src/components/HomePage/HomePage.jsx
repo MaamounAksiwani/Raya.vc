895043e96c7c093c2158dacf6dad2ec1424d6993
System Administrator <root@Maamouns-MacBook-Pro.local>
2024-06-18 12:54:15 +0300

    edit the home page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -4,6 +4,8 @@ import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import fullpage from 'fullpage.js';
 import FirstSection from '../FirstSection/FirstSection';
 import SecondSection from '../SecondSection/SecondSection';
+import { Container } from '@mui/material';
+
 import WOW from 'wowjs';
 
 import ThirdSection from '../ThirdSection/ThirdSection';
@@ -18,11 +20,11 @@ const HomePage = () => {
         const fullpageInstance = new fullpage('#fullpage', {
             sectionSelector: '.section',
             scrollBar: false,
-            scrollingSpeed: 1000, // Set scrolling speed to 1 second (1000 milliseconds)
+            scrollingSpeed: 1000,
             touchSensitivity: 15,
             credits: { enabled: false },
             navigation: false,
-            
+
 
             afterLoad: (origin, destination, direction) => {
                 // Check if auto-scrolling is enabled and user has scrolled up
@@ -67,52 +69,57 @@ const HomePage = () => {
     useEffect(() => {
         document.title = "Home | Raya";
         return () => {
-          document.title = "";
+            document.title = "";
         };
-      }, []);
+    }, []);
 
     return (
         <>
-        <div id='fullpage'>
-            <div className='section'>
-                <div className="background">
-                    <video data-keepplaying="true" playsinline="" autoplay="" loop muted class="show hero__video hero__background-video hero__height">
-                        <source src="https://cdn.sanity.io/files/ti7si9cx/production/062ad65624568ca35aa36ed0abbaa5d072d678a1.mp4" type="video/mp4" />
-                    </video>
-                    <div className="overlay"></div>
-                    <div className="centered-text">
-                        <h1>MAKE HISTORY</h1>
-                    </div>
-                    <div className="bottom-text">
-                        <ArrowDownwardIcon
-                            className='arrow'
-                            style={{ fontSize: '30px', border: '1px solid #000', marginBottom: '30px', borderRadius: '50%', padding: '2px' }}
-                            onClick={moveNextSection}
-                        />
+            <div id='fullpage'>
+
+                <div className='section'>
+                    <div className="background">
+                        <video data-keepplaying="true" playsinline="" autoplay="" loop muted class="show hero__video hero__background-video hero__height">
+                            <source src="https://cdn.sanity.io/files/ti7si9cx/production/062ad65624568ca35aa36ed0abbaa5d072d678a1.mp4" type="video/mp4" />
+                        </video>
+                        <div className="overlay"></div>
+                        <div className="centered-text">
+                            <h1>MAKE HISTORY</h1>
+                        </div>
+                        <div className="bottom-text">
+                            <ArrowDownwardIcon
+                                className='arrow'
+                                style={{ fontSize: '30px', border: '1px solid #000', marginBottom: '30px', borderRadius: '50%', padding: '2px' }}
+                                onClick={moveNextSection}
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
+                <Container maxWidth='xl'>
+
+                    <div class="section" data-percentage="80" data-centered="true">
+                        <FirstSection />
+                    </div>
+
+                    <div class="section" data-percentage="80" data-centered="true">
+                        {/* <SecondSection /> */}
+                        <FirstSection />
+                    </div>
+
+                    <div class="section" data-percentage="80" data-centered="true">
+                        {/* <ThirdSection /> */}
+                        <FirstSection />
+                    </div>
+
+                    <div className='section'>
+                        <FirstSection />
+                        {/* <FourthSection /> */}
+                    </div>
+
+                </Container>
 
 
-            <div class="section" data-percentage="80" data-centered="true">
-                <FirstSection />
             </div>
-
-            <div class="section" data-percentage="80" data-centered="true">
-                {/* <SecondSection /> */}
-                <FirstSection />
-            </div>
-
-            <div class="section" data-percentage="80" data-centered="true">
-                {/* <ThirdSection /> */}
-                <FirstSection />
-            </div>
-
-            <div className='section'>
-            <FirstSection />
-                {/* <FourthSection /> */}
-            </div>
-        </div>
 
         </>
     );
